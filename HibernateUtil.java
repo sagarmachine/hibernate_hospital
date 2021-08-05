056de2190f20268b666fc5327ddf04260a532f44
sagarmachine <sagarmonkey800@gmail.com>
2021-08-05 14:29:35 +0530

    Update HibernateUtil.java

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -10,13 +10,13 @@ try {
 				Configuration configuration = new Configuration();
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/patientinfo");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hospital");
 				settings.put(Environment.USER, "root");
-				settings.put(Environment.PASS, "");
+				settings.put(Environment.PASS, "root");
 				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 				settings.put(Environment.SHOW_SQL, "true");
                  settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "create");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
                 configuration.setProperties(settings);
                 configuration.addAnnotatedClass(Doctor.class);
              //   configuration.addAnnotatedClass(Payment.class);
